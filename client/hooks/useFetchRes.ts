5a012189c049db6c5cd7e75f40e89108dbe36731
Saikat-D023 <saikatdey358@gmail.com>
2026-02-11 20:23:55 +0530

    journal data displayed in UI

--- client/hooks/useFetchRes.ts
+++ client/hooks/useFetchRes.ts
@@ -1,15 +1,18 @@
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
 
-type ResData = {
-    message: string
+type journalData = {
+    title: string,
+    content: string,
+    mood: string,
+    summary: string
 }
 
 export const useFetchRes = () => {
     return useQuery({
-        queryKey: ['ResData'],
+        queryKey: ['journalData'],
         queryFn: () => {
-            return axios.get<ResData>("http://localhost:3000/api/testing")
+            return axios.get<journalData[]>("http://localhost:3000/api/journal")
         },
         staleTime: 1000,
     })
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,39 @@ import { useFetchRes } from '../hooks/useFetchRes'
 
 function App() {
   const { data, error, isFetching, isLoading } = useFetchRes();
-  if(isLoading || isFetching) return <div>Loading...</div>
-  if(error) return <div>{error.message}</div>
+  if (isLoading || isFetching) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("submitted");
+  }
   return (
     <>
       <div>
-        <h2>testing the server here:</h2>
-        <p>{data?.data.message}</p>
+        <div>
+          <form>
+            <input type="text" placeholder='title' />
+            <input type="text" placeholder='content' />
+            <input type="text" placeholder='mood' />
+            <input type="text" placeholder='summary' />
+            <button type='submit'
+              onClick={handleSubmit}>Add Journal</button>
+          </form>
+        </div>
+
+        <h2>The journal datas are</h2>
+        {data?.data.map((item) => {
+          return (
+            <>
+              <h3>Title: {item.title}</h3>
+              <p>Content: {item.content}</p>
+              <p>Mood: {item.mood}</p>
+              <p>Summary: {item.summary}</p>
+            </>
+          )
+        })}
       </div>
     </>
   )
 }
-
 export default App
